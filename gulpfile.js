9ac0aec996bcfbdebb2a5e6fb8a954f1e9fd9a3d
Chris Reidy <christopher.t.reidy@googlemail.com>
2016-11-13 23:39:51 +0000

    Reconfigure gulp to include gulp-connect-php

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,24 @@
-// https://css-tricks.com/gulp-for-beginners/
-
-// So after installing gulp globally: sudo npm install gulp -g
-// Then running `npm init` in our project to create the package.json
-// And installing Gulp into the project locally: npm install gulp --save-dev
-// --save-dev ensures gulp gets added to devDependencies in package.json
-// dependencies are required to run, devDependencies only to develop
-
-// I also installed the plugin gulp-sass - npm install gulp-sass --save-dev
-// And npm install browser-sync --save-dev (no gulp- prefix as Browser Sync works with Gulp so no plugin required)
-
-
-// require tells Node to look in node_modules for package named gulp
+// As browserSync's server can only deal with static files, I'm using gulp-connect-php
+// We tell browserSync to forward all calls from localhost:8080 to 127.0.0.1:8000,
+// which is 127.0.0.1 (usually same as localhost) plus the port on which you want to access the webserver (8000).
+// This won't work if 8000 is already in use
+// This doesn't rely on MAMP - gulp-connect-php uses the built-in server in PHP 5.4.0+
 var gulp = require('gulp');
+var connect = require('gulp-connect-php');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-// We need to create a browsersync task to enable Gulp to spin up a web server (for development) using BrowserSync.
-// As we're running a server, we need to let Browser Sync know where the root of the server should be.
-// In our case it's the home folder
-gulp.task('browserSync', function(){
-	browserSync.init({
-		server: {
-			baseDir: ''
-		},
-	})
+var browserSync = require('browser-sync');
+
+gulp.task('connect', function() {
+    connect.server({}, function (){
+	    browserSync({
+	    	proxy: '127.0.0.1:8000',
+	     	port: 8080,
+        	open: true,
+        	notify: false
+	    });
+	  });
 });
 
-// These tasks can also be run in the command line - e.g. gulp hello
-gulp.task('hello', function(){
-	console.log('hi there');
-});
-
-// Tasks are normally more complex, with 2 additional Gulp methods and a variety of Gulp plugins
 gulp.task('sass', function(){
 	return gulp.src('sass/main.scss') // gulp.src tells Gulp which files to use
 		.pipe(sass()) // sends it through a gulp plugin
@@ -40,26 +28,8 @@ gulp.task('sass', function(){
 		}))
 });
 
-// You can use node globs (matching patterns) for file paths if you want
-// e.g. **/*.scsss matches any file ending with .scss in root folder and any child directories
-// *.+(scss|sass) would match any file ending with .scss or .sass in root folder
-
-// You could watch one type of file individually. 
-gulp.watch('sass/**/*.scss', ['sass']); 
-
-// But usually, you want mulitple watch processes. So you can group them into a watch task.
-// Also we can run watch and browserSync together
-// This tells watch task that browserSync must be completed before watch is allowed to run
-// We also want sass to run before watch, so the CSS will already be the latest whenever we run a Gulp command
-// So if I run gulp watch in command line...
-// ... Gulp will start browserSync and sass tasks concurrently...
-// ... And when both tasks are completed, only then will watch run.
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('default', ['connect', 'sass'], function(){
 	gulp.watch('sass/**/*.scss', ['sass']);
-	// Then other watchers would go here, which call on BS's reload function
-	gulp.watch('index.html', browserSync.reload);
+	gulp.watch(['index.php', 'viewModules/*.php'], browserSync.reload);
 	gulp.watch('js/**/*.js', browserSync.reload);
 });
-
-
-
