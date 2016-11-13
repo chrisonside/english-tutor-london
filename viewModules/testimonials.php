7cc2158dee13ed01cf69685288163421c33b66f2
Chris Reidy <christopher.t.reidy@googlemail.com>
2016-11-13 23:38:55 +0000

    Create partials structure via PHP includes

---
+++ viewModules/testimonials.php
@@ -0,0 +1,63 @@
+<section id="testimonials" class="testimonials">
+            
+    <div class="testimonial testimonial--one">
+        <div class="testimonial__text">
+            <div class="testimonial__quote">
+                &ldquo;Will always prepared with customised materials which helped me a lot in real life situations.&rdquo;
+            </div>
+            <div class="testimonial__source">
+                J&#243;zsef Harcsa
+            </div>
+            <div class="testimonial__job">
+                Systems Analyst
+            </div>
+        </div>
+    </div>
+
+    <div class="testimonial testimonial--two">
+        <div class="testimonial__text">
+            <div class="testimonial__quote">
+                &ldquo;You get more than just 1-2-1 sessions - Will is expert at structuring extra-curricular work that really speeds up the improvements through home learning in the employee's own time.&rdquo;
+            </div>
+            <div class="testimonial__source">
+                Dr Craig Fergusson
+            </div>
+            <div class="testimonial__job">
+                Associate Director, Human Resources
+            </div>
+            <div class="testimonial__company">
+                Principles for Responsible Investment
+            </div>
+        </div>
+    </div>
+    <div class="testimonial testimonial--three">
+        <div class="testimonial__text">
+            <div class="testimonial__quote">
+                &ldquo;If you need to brush up on your English and sound like a native, then you've come to the right man.&rdquo;
+            </div>
+            <div class="testimonial__source">
+                Mariana Garcia
+            </div>
+            <div class="testimonial__job">
+                Plastic Surgeon
+            </div>
+            <div class="testimonial__company">
+                Buenos Aires
+            </div>
+        </div>
+    </div>
+    <div class="testimonial testimonial--four">
+        <div class="testimonial__text">
+            <div class="testimonial__quote">
+                &ldquo;Working with Will is undoubtedly my best English learning experience ever!&rdquo;
+            </div>
+            <div class="testimonial__source">
+                Ashley Hsuan-Wen Hwang
+            </div>
+            <div class="testimonial__job">
+                Job Title tbc
+            </div>
+        </div>
+    </div>
+
+</section>
