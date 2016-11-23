d25779c1d235830f51bd3d51e90ba566fb92df34
Chris Reidy <christopher.t.reidy@googlemail.com>
2016-11-23 16:04:01 +0000

    Remove gulp-connect-php Gulp set-up

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,16 @@
-// As browserSync's server can only deal with static files, I'm using gulp-connect-php
-// We tell browserSync to forward all calls from localhost:8080 to 127.0.0.1:8000,
-// which is 127.0.0.1 (usually same as localhost) plus the port on which you want to access the webserver (8000).
-// This won't work if 8000 is already in use
-// This doesn't rely on MAMP - gulp-connect-php uses the built-in server in PHP 5.4.0+
 var gulp = require('gulp');
-var connect = require('gulp-connect-php');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 
-gulp.task('connect', function() {
-    connect.server({}, function (){
-	    browserSync({
-	    	proxy: '127.0.0.1:8000',
-	     	port: 8080,
-        	open: true,
-        	notify: false
-	    });
-	  });
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: ''
+		},
+	})
 });
 
+// Tasks are normally more complex, with 2 additional Gulp methods and a variety of Gulp plugins
 gulp.task('sass', function(){
 	return gulp.src('sass/main.scss') // gulp.src tells Gulp which files to use
 		.pipe(sass()) // sends it through a gulp plugin
@@ -28,8 +20,8 @@ gulp.task('sass', function(){
 		}))
 });
 
-gulp.task('default', ['connect', 'sass'], function(){
+gulp.task('default', ['browserSync', 'sass'], function(){
 	gulp.watch('sass/**/*.scss', ['sass']);
-	gulp.watch(['index.html', 'viewModules/*.php'], browserSync.reload);
+	gulp.watch('index.html', browserSync.reload);
 	gulp.watch('js/**/*.js', browserSync.reload);
 });
